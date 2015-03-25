1b421e58f2fcb330ecb7f3bec8636f619bd49fc8
zhong2tao <zhongt@bingosoft.net>
2015-03-25 10:45:08 +0800

    .net客户端初次版本提交

---
+++ net/Link.Message.Client/content/InvokeType.cs
@@ -0,0 +1,14 @@
+﻿namespace Link.Message.Client.content
+{
+
+	/// <summary>
+	/// 调用类型
+	/// </summary>
+	public enum InvokeType
+	{
+		DoNothing         = 1,
+		InvokeCustomApp   = 2,
+		InvokeChatWindow  = 3
+	}
+
+}
